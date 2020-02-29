600138b739f71662eb8332d54680429bf4ef5772
Ethan <ethan_gustafson@outlook.com>
2020-02-28 20:59:00 -0500

    Done.

--- index.js
+++ index.js
@@ -38,3 +38,16 @@ titleCased(tutorials);
 "What Is The Difference Between == And ===?", 
 "What Is The Difference Between Event Capturing And Bubbling?", 
 "What Is Jsonp?"] */
+
+/* but the test expects this instead:
+
+["What Does The This Keyword Mean?",
+"What Is The Contutorialuctor OO Pattern?",
+"Implementing Blockchain Web API",
+"The Test Driven Development Workflow",
+"What Is NaN And How Can We Check For It",
+"What Is The Difference Between StopPropagation And PreventDefault?",
+"Immutable State And Pure Functions",
+"What Is The Difference Between == And ===?",
+"What Is The Difference Between Event Capturing And Bubbling?",
+"What Is JSONP?"] */
