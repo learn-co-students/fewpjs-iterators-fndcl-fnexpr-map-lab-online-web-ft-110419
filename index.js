759b5ef6472d3651254871eac4de1931bb745916
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-03-03 19:12:24 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+    let newTuto = tutorials.map(capatalize)
+    return newTuto
+}
+
+function capatalize(sentence) {
+    return sentence.split(' ').map(w => w.charAt(0).toUpperCase() + w.substring(1)).join(' ');
 }
