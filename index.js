63dfc22da51181b603d1ee431247349f796a2e92
nkizito92 <nkizito57@gmail.com>
2020-03-11 15:39:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
 const tutorials = [
-  'What Does The This Keyword Mean?',
-  'What Is The Contutorialuctor OO Pattern?',
-  'Implementing Blockchain Web API',
+  'what does the this keyword mean?',
+  'What is the Contutorialuctor OO pattern?',
+  'implementing Blockchain Web API',
   'The Test Driven Development Workflow',
-  'What Is NaN And How Can We Check For It',
-  'What Is The Difference Between StopPropagation And PreventDefault?',
-  'Immutable State And Pure Functions',
-  'What Is The Difference Between == And ===?',
-  'What Is The Difference Between Event Capturing And Bubbling?',
-  'What Is JSONP?'
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (str) => {
+  return tutorials.map(word => {
+    let words = word.split(' ')
+    let capitalizedWords =
+      words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    let res = capitalizedWords.join(' ')
+    return res
+  })
 }
-
-let newTitle = tutorials.map(string => string)
-
-
-// const tutorials = [
-//   'what does the this keyword mean?',
-//   'What is the Contutorialuctor OO pattern?',
-//   'implementing Blockchain Web API',
-//   'The Test Driven Development Workflow',
-//   'What is NaN and how Can we Check for it',
-//   'What is the difference between stopPropagation and preventDefault?',
-//   'Immutable State and Pure Functions',
-//   'what is the difference between == and ===?',
-//   'what is the difference between event capturing and bubbling?',
-//   'what is JSONP?'
-// ];
-
-// const titleCased = (str) => {
-//   let arr = str.join(" ").split(" ")
-//   let newArr = [];
-//   for (let x = 0; x < arr.length; x++) {
-//     newArr.push(arr[x].charAt(0).toUpperCase() + arr[x].slice(1))
-//   }
-//   return newArr.join(" ");
-// }
